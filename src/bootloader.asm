b8d8c056e46d16005fd9240b75711ac2a5b6ffac
E. Almqvist <elalmqvist@gmail.com>
2021-01-27 11:18:00 +0100

    Added testing string

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -6,6 +6,9 @@ call println
 mov bx, infoString
 call println
 
+mov bx, hexTestPrefixString
+call print
+
 mov dx, 0x002e ; test the conversion
 call hexToASCII
 call println
@@ -22,8 +25,9 @@ jmp $  ; inf loop
 %include "elib/convert.asm"
 
 ;; Data
-welcomeString: db "Welcome to: e Operating-System (eOS)", ASCII_END
-infoString: db    "Version 2021 0.0", ASCII_END
+welcomeString:		db "Welcome to: e Operating-System (eOS)", ASCII_END
+infoString: 		db "Version 2021 0.0", ASCII_END
+hexTestPrefixString:	db "Hex printing test: ", ASCII_END
 
 ; Magic BIOS number 
 times 510-($-$$) db 0
