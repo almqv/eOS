9e05fcf9a26fdc09014f374270e7c50c11932150
E. Almqvist <elalmqvist@gmail.com>
2021-01-27 11:14:32 +0100

    Removed comment

--- src/elib/convert.asm
+++ src/elib/convert.asm
@@ -4,8 +4,6 @@ HEX_OUT: db "0x0000", ASCII_END
 ; This SR is going to mess up some registers
 ; since it fills the register with the desired 
 ; pointer toward the string.
-;
-; Disclaimer: use popa if you want to get back your registers!!!
 
 hexToASCII:
 	mov cx, 0 ; incrementor
