9b32990c6584517117c26eee43f4cf5453a50482
E. Almqvist <elalmqvist@gmail.com>
2021-05-21 10:05:14 +0200

    Minor typo fix

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ start_bootloader() {
 	qemu-system-x86_64 $bootloader
 }
 
-if test -f "$PWD/$bootloader" then
+if test -f "$PWD/$bootloader"; then
 	start_bootloader
 else
 	/usr/bin/bash $PWD/compile.sh && start_bootloader
