397c272175811a810b78534db9215222115ec6f1
E. Almqvist <elalmqvist@gmail.com>
2021-08-08 19:23:45 +0200

    Color buffer

--- src/pm/vga/print.asm
+++ src/pm/vga/print.asm
@@ -3,13 +3,15 @@
 ; VGA base address: 0xb8000
 ; Charpos = 0xb8000 + 2(row*80 + col)
 
+vga_color_buf: db 0x0f		; Buffer to be changed so that we can choose colors!
+
 vga_print:
 	pusha
 	mov edx, VIDEO_MEM
 
 	vga_print_loop:
 		mov al, [ebx]		; Pointer to char
-		mov ah, 0x8f		; Color code
+		mov ah, [vga_color_buf]	; Color code
 
 		cmp al, ASCII_END	; Check if end of string
 		je vga_print_return	; If end then return
