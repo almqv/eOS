aa38917ca8767dd0184d6bb4207fee4e39e41cee
E. Almqvist <elalmqvist@gmail.com>
2021-08-12 12:34:53 +0200

    File structure refactor

--- compile.sh
+++ compile.sh
@@ -7,10 +7,10 @@ srcDir=src
 binDir=bin
 
 echo "Compiling bootloader... "
-nasm -f bin $srcDir/bootloader.asm -i $srcDir -o $binDir/bootloader.bin 
+nasm -f bin $srcDir/bootloader/bootloader.asm -i $srcDir/bootloader -o $binDir/bootloader.bin 
 
 echo "Compiling kernel... "
-gcc -fno-pie -m32 -Os -ffreestanding -c $srcDir/kernel.c -o $binDir/kernel.o && gcc -o $binDir/kernel.bin $binDir/kernel.o -Wl,--oformat=binary -ffreestanding -nostdlib -shared -Ttext 0x1000 -m32 
+gcc -fno-pie -m32 -Os -ffreestanding -c $srcDir/kernel/kernel.c -o $binDir/kernel.o && gcc -o $binDir/kernel.bin $binDir/kernel.o -Wl,--oformat=binary -ffreestanding -nostdlib -shared -Ttext 0x1000 -m32 
 
 echo "Creating OS image... "
 cat $binDir/bootloader.bin $binDir/kernel.bin > $binDir/os-image 
