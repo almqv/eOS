89e8d604a2381211d5c0a2f45b19123e98956469
E. Almqvist <elalmqvist@gmail.com>
2021-08-16 22:17:51 +0200

    Cleanup

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,11 +1,7 @@
 #include "lib/vga.c"
 
 void main() {
-	// Do kernel stuff
-	char* vidmem = (char*)0xb8000;
-	*vidmem = 'X';
-
 	clear_screen();
 	set_cursor_pos(24, 0);
-	println("\t eOS Version 0.0 2021", 0xf0);
+	println("eOS Version 0.0 2021", 0xf0);
 }
