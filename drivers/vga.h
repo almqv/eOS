46c231a4c591a6477e3ca9bcd9aa67d6a0813e1b
E. Almqvist <elalmqvist@gmail.com>
2021-08-29 13:20:35 +0200

    printalign & alignment enum etc

--- drivers/vga.h
+++ drivers/vga.h
@@ -1,7 +1,19 @@
+#define VGA_ADDRESS 	(char*)0xb8000
+#define VGA_ADDRESS_MAX	(char*)0xb8fa0
+
+#define MAX_ROWS 25
+#define MAX_COLS 80
+
+static unsigned int cursor_row;
+static unsigned int cursor_col;
+
+enum align {LEFT, RIGHT, MIDDLE};
+
 char* get_memory_charpos(unsigned int col, unsigned int row);
 void writechar(char c, unsigned int col, unsigned int row, int colorcode);
 void clear_screen();
 void set_cursor_pos();
 void print();
 void println();
+void printalign(char* str, int attribute_byte, enum align alignment);
 void vga_init();
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,13 +6,18 @@ void main() {
 	vga_init(); 	// Initialize the screen first
 			// i.e. clear the screen et cetera.
 
-	set_cursor_pos(28, 2);
-	print("eOS Version 0.1 2021", 0x0f);
+	char* title = "eOS Version 0.1 2021";
+	set_cursor_pos(0, 2);
+	printalign(title, 0xf0, MIDDLE);
+
+	char* subtitle = "A x86 operating system, licenced under GPL-2.0";
+	set_cursor_pos(0, 3);
+	printalign(subtitle, 0xf8, MIDDLE);
 
 
 	char* strbuf = "Hello";
 	char* str2 = "World!";
 	strbuf = strcat(strbuf, str2); 
 	set_cursor_pos(0, 0);
-	println(strbuf, 0x0f);
+	println(strbuf, 0xf0);
 }
