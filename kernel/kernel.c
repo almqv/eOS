d41dea837bb78eccaf00a5beef9c40dbfe10f29a
E. Almqvist <elalmqvist@gmail.com>
2022-01-07 22:05:50 +0100

    Stuff

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -25,6 +25,9 @@ void init() {
 	println("0x1000", DEFAULT_COLOR);
 	*/
 
+	char* malloctest = pm_malloc(2); // allocate two blocks
+
+
 
 	char* intbuf = "xxxx";
 	int num = 1234;
--- kernel/memory.c
+++ kernel/memory.c
@@ -113,7 +113,7 @@ pointer pm_malloc(uint block_count) {
 	lower = find_free(block_count);
 
 	if( lower < 0 )
-		println("malloc: OUT OF MEMORY", URGENT_COLOR);
+		println("malloc: OUT OF MEMORY :(", URGENT_COLOR);
 		// do some out-of-memory interupt
 		return 0x0;
 
