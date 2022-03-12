bb34ec27adc89e29b0e7ee34a7b69f90f92cb34d
E. Almqvist <elalmqvist@gmail.com>
2022-03-12 17:11:28 +0100

    Stuff

--- kernel/kernel_entry.asm
+++ kernel/kernel_entry.asm
@@ -1,8 +1,18 @@
 [bits 32]
 [extern kernel_init]
 
-multiboot2:
-	dw 0xE85250D6
+MBOOT_PAGE_ALIGN        equ 1<<0
+MBOOT_MEMORY_INFO       equ 1<<1
+MBOOT_HEADER_MAGIC      equ 0x1BADB002
+MBOOT_HEADER_FLAGS      equ MBOOT_PAGE_ALIGN | MBOOT_MEMORY_INFO
+MBOOT_CHECKSUM          equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
+
+;align 4
+multiboot_header:
+	dd MBOOT_HEADER_MAGIC
+    dd MBOOT_HEADER_FLAGS
+    dd MBOOT_CHECKSUM
+	
 
 call kernel_init 
 jmp $
