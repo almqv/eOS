f51c3c7bbe407bab9c3f9005699e0299d32a47ac
E. Almqvist <elalmqvist@gmail.com>
2022-03-15 21:50:22 +0100

    Comment out stuff

--- bootloader/bios/memory.asm
+++ bootloader/bios/memory.asm
@@ -1,13 +1,13 @@
 ; SRs to find memory size and leave it as a "note" for the kernel
 
 ; Descriptor Table for address ranges
-addr_rng_dt_start:	
-	addr_rng_low: dd 0
-	addr_rng_high: dd 0
-	addr_rng_len_low: dd 0
-	addr_rng_len_high: dd 0
-	addr_rng_type: dd 0
-addr_eng_dt_end:
+;addr_rng_dt_start:	
+;	addr_rng_low: dd 0
+;	addr_rng_high: dd 0
+;	addr_rng_len_low: dd 0
+;	addr_rng_len_high: dd 0
+;	addr_rng_type: dd 0
+;addr_eng_dt_end:
 
 map_phys_mem_init:
 	mov ebx, 0
