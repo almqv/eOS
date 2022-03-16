2942b2e37ee0b1394615565b4335cf24bd8bc44a
E. Almqvist <elalmqvist@gmail.com>
2022-03-16 09:20:59 +0100

    Comp

--- bootloader/bios/memory.asm
+++ bootloader/bios/memory.asm
@@ -9,12 +9,12 @@
 ;	addr_rng_type: dd 0
 ;addr_eng_dt_end:
 
-map_phys_mem_init:
-	mov ebx, 0
-map_phys_mem:
-	mov eax, 0xe820 ; Function code
-	mov es, 
-	mov edx, 'SMAP' ; Signature
-	int BIOS_PMEM_INT
+;map_phys_mem_init:
+;	mov ebx, 0
+;map_phys_mem:
+;	mov eax, 0xe820 ; Function code
+;	mov es, 
+;	mov edx, "SMAP" ; Signature
+;	int BIOS_PMEM_INT
 
 
