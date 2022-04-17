dd261b623af7d7742a9d1653618870b497f5b5d6
E. Almqvist <elalmqvist@gmail.com>
2022-04-18 00:08:56 +0200

    Cleanup

--- kernel/interupt.c
+++ kernel/interupt.c
@@ -1,18 +1,31 @@
 #include "interupt.h"
 
-struct InteruptDescriptor {
+typedef struct {
 	uint16	offset_1;	// offset 0 to 15 bits
 	uint16	selector;	// code segment sel
-	uint8	null;		// "padding", should be zero
+	uint8	reserved;	// should be zero
 	uint8	type_attr;	// type & attr stuff
 	uint16	offset_2;	// offset 16 to 31
-};
+} __attribute__((packed)) idt_entry;
+
+typedef struct {
+	uint16	limit;
+	uint	base;
+} __attribute__((packed)) idtr;
 
 void init_IDT(uint type_attr) {
-	__asm__ __volatile__("lgdt %[idt_pointer]");
+	__attribute__((aligned(4)))
+	static idt_entry IDT[256];
+	static idtr	IDTR;
 }
 
-void int_handler() {
+__attribute__((noreturn))
+void exception_handler(void);
+void exception_handler() {
+	__asm__ __volatile__("cli; hlt");
+}
+
+void interupt_handler() {
 	__asm__("pusha");
 	// handle stuff
 	__asm__("popa; leave; iret");
